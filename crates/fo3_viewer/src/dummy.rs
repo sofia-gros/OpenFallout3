e89b29b4b17e515281bac1179e70ba8d187d5e86
sofia-gros <sofiagros4@gmail.com>
2026-09-17 15:11:25 +0900

    Chore: Remove dummy.rs

--- crates/fo3_viewer/src/dummy.rs
+++
@@ -1,8 +0,0 @@
-    pub fn load_streaming_cells(
-        &self,
-        new_grids: &[(i32, i32)],
-        viewer_state: &mut crate::app::ViewerState,
-    ) {
-        // Phase 3: TODO 実際のストリーミングロード処理
-        println!("TODO: Streaming cells for grids: {:?}", new_grids);
-    }
